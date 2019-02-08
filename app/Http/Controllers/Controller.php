3597ba9c3b9d119a3959dbc226aebb76aae67f55
tarikul <tarik.md91@gmail.com>
2019-02-08 09:27:08 -0800

    Add Test3 method

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -12,4 +12,5 @@ class Controller extends BaseController
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
     function test(){}
     function test2(){}
+    function test3() {} // i add test3 method
 }
