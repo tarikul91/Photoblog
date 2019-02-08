6192b92e22b2a205f93a0909eb874aa334272646
tarikul <tarik.md91@gmail.com>
2019-02-08 09:01:58 -0800

    i have removed navbar.

--- resources/views/photo/another.blade.php
+++ resources/views/photo/another.blade.php
@@ -8,70 +8,7 @@
 
 	</head>
 	<body>
-		<nav class="navbar navbar-default">
-			<div class="container-fluid">
-				<!-- Brand and toggle get grouped for better mobile display -->
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-					<span class="sr-only">Toggle navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					</button>
-					<a class="navbar-brand" href="#">PhotoBlog</a>
-				</div>
-				<!-- Collect the nav links, forms, and other content for toggling -->
-				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-					<ul class="nav navbar-nav">
-						<li><a href="/photoblog/add">Add Photo</a></li>
-						<li><a href="/photoblog/group/add">Add Group</a></li>
-						<li><a href="#">Mark</a></li>
-
-						
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Action<span class="caret"></span></a>
-							<ul class="dropdown-menu">
-								<li><a href="#">Delete</a></li>
-								<li><a href="#">Move</a></li>
-								<li role="separator" class="divider"></li>
-								<li><a href="#">Add to Group</a></li>
-							</ul>
-						</li>
-
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Gallery<span class="caret"></span></a>
-							<ul class="dropdown-menu">
-								<li><a href="#">Small Size</a></li>
-								<li><a href="#">Medium size</a></li>
-								<li><a href="#">Large size</a></li>
-							   <li><a href="#">Extra large </a></li>
-							</ul>
-						</li>
-
-					</ul>
-					
-					<ul class="nav navbar-nav navbar-right">
-						
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">User Name <span class="caret"></span></a>
-							<ul class="dropdown-menu">
-								<li><a href="#">Profile</a></li>
-								<li><a href="#">Logout</a></li>
-							</ul>
-						</li>
-						<li>
-							<div class="inputgrpcontainer">
-								  <input type="text" name="search" value="Type..." class="inputform">
-								 <div class="srchicon">
-								 <i class="fa fa-search icon" aria-hidden="true">		</i>
-							</div>
-					   </li>
-					</ul>
-				</div>
-				<!-- /.navbar-collapse -->
-			</div>
-			<!-- /.container-fluid -->
-		</nav>
+		
 		<div class="mrow">
 			<div class="flush-message">
 				@if( session()->get("message") )
